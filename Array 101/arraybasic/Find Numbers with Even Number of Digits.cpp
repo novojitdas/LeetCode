1b1ed5b3a4028acf96d8f890c13728a357c7aedc
Novojit Das <38981630+novojitdas@users.noreply.github.com>
2021-12-15 20:53:42 +0600

    day 1

---
+++ Array 101/arraybasic/Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfDigits(int n){
+        int counter = 0;
+        while(n>0){
+            n = n/10;
+            counter++;
+        }
+        return counter;
+    }
+    
+    int findNumbers(vector<int>& nums) {
+        
+        int even = 0;
+        for(int i: nums)
+        {
+            if(numberOfDigits(i)%2==0)
+            {
+                even++;
+            }
+        }
+        
+       return even; 
+    }
+};
